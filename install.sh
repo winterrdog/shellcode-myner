c0cbb597ec77e307e152a2d9b6b7afe52da4ee23
winterrdog <87463258+winterrdog@users.noreply.github.com>
2022-03-09 10:23:41 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
 cargo build --release
+mv -u ./target/release/shellcode-myner .
+
 printf "\e[1;96m \n[+] Now, you can run shellcode-myner like this:\n \e[0m"
-printf "\e[1;93m \t./target/release/shellcode-myner <binary_to_inspect> \e[0m"
+printf "\e[1;93m \t./shellcode-myner <binary_to_inspect> \e[0m"
