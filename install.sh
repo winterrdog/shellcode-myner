560d5d934b359a1756cd0bd089f557ca952d6f26
winterrdog <87463258+winterrdog@users.noreply.github.com>
2022-04-23 11:49:57 +0300

    Added `cargo clean` to remove artifacts.

--- install.sh
+++ install.sh
@@ -3,5 +3,8 @@
 cargo build --release
 mv -u ./target/release/shellcode-myner .
 
-printf "\e[1;96m \n[+] Now, you can run shellcode-myner like this:\n \e[0m"
-printf "\e[1;93m \t./shellcode-myner <binary_to_inspect> \e[0m"
+printf "\e[1;96m \n[+] Removing artifacts...\n \e[0m"
+cargo clean
+
+printf "\e[1;96m \n[+] Now, you can run shellcode-myner like this to find out about its usage:\n \e[0m"
+printf "\e[1;93m \t./shellcode-myner -h \e[0m"
