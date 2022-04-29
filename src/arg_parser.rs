97cd3f6463871ad8c616234ae841b2355b2b677d
winterrdog <winterrdog@keemail.me>
2022-04-29 21:56:16 +0300

    Added license notice.

--- src/arg_parser.rs
+++ src/arg_parser.rs
@@ -1,3 +1,28 @@
+/*
+    shellcode-myner
+    A tool used to extract shellcode from an object/binary file.
+
+    Copyright (C) 2022  winterrdog
+
+    This file is part of shellcode-myner.
+
+    shellcode-myner is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    shellcode-myner is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+    CONTACT:
+        - Email: winterrdog@protonmail.ch
+        - Telegram: https://t.me/winterrdog
+*/
 use clap::{self, arg, Arg};
 
 #[must_use]
